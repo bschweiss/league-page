46cb72ff7f7d07ab3ebb5445132559a9450b8053
bschweiss <benjaminschweiss@gmail.com>
2025-09-12 19:33:08 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1122280530797637632"; // your league ID
+export const leagueID = "1257432772197236736"; // your league ID
 export const leagueName = "Imagination Football"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
